5188d1506a454a8300f0b3a7d7acd52e44d8261c
yongenaelf <132553186+yongenaelf@users.noreply.github.com>
2024-07-07 01:49:06 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,23 +4,26 @@ import (
 	"bufio"
 	"context"
 	"fmt"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
 	"math/big"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
+
+	"github.com/AElfProject/aelf-sdk.go/client"
 )
 
 var (
-	allWatching []*Watching
-	port        string
-	updates     string
-	prefix      string
-	loadSeconds float64
-	totalLoaded int64
-	eth         *ethclient.Client
+	allWatching  []*Watching
+	port         string
+	updates      string
+	prefix       string
+	loadSeconds  float64
+	totalLoaded  int64
+	aelfClient   *client.AElfClient
+	sleepSeconds int
 )
 
 type Watching struct {
@@ -29,45 +32,23 @@ type Watching struct {
 	Balance string
 }
 
-//
-// Connect to geth server
-func ConnectionToGeth(url string) error {
+// Connect to AElf node
+func ConnectToAElf(url string) error {
 	var err error
-	eth, err = ethclient.Dial(url)
+	aelfClient, err = client.NewAElfClient(url)
 	return err
 }
 
-//
-// Fetch ETH balance from Geth server
-func GetEthBalance(address string) *big.Float {
-	balance, err := eth.BalanceAt(context.TODO(), common.HexToAddress(address), nil)
+// Fetch balance from AElf node
+func GetAElfBalance(address string) *big.Float {
+	balance, err := aelfClient.GetBalance(context.TODO(), address, "ELF")
 	if err != nil {
-		fmt.Printf("Error fetching ETH Balance for address: %v\n", address)
+		log.Printf("Error fetching balance for address: %v\n", address)
+		return big.NewFloat(0)
 	}
-	return ToEther(balance)
+	return new(big.Float).SetFloat64(balance.Balance)
 }
 
-//
-// Fetch ETH balance from Geth server
-func CurrentBlock() uint64 {
-	block, err := eth.BlockByNumber(context.TODO(), nil)
-	if err != nil {
-		fmt.Printf("Error fetching current block height: %v\n", err)
-		return 0
-	}
-	return block.NumberU64()
-}
-
-//
-// CONVERTS WEI TO ETH
-func ToEther(o *big.Int) *big.Float {
-	pul, int := big.NewFloat(0), big.NewFloat(0)
-	int.SetInt(o)
-	pul.Mul(big.NewFloat(0.000000000000000001), int)
-	return pul
-}
-
-//
 // HTTP response handler for /metrics
 func MetricsHttp(w http.ResponseWriter, r *http.Request) {
 	var allOut []string
@@ -79,16 +60,15 @@ func MetricsHttp(w http.ResponseWriter, r *http.Request) {
 		bal := big.NewFloat(0)
 		bal.SetString(v.Balance)
 		total.Add(total, bal)
-		allOut = append(allOut, fmt.Sprintf("%veth_balance{name=\"%v\",address=\"%v\"} %v", prefix, v.Name, v.Address, v.Balance))
+		allOut = append(allOut, fmt.Sprintf("%vaelf_balance{name=\"%v\",address=\"%v\"} %v", prefix, v.Name, v.Address, v.Balance))
 	}
-	allOut = append(allOut, fmt.Sprintf("%veth_balance_total %0.18f", prefix, total))
-	allOut = append(allOut, fmt.Sprintf("%veth_load_seconds %0.2f", prefix, loadSeconds))
-	allOut = append(allOut, fmt.Sprintf("%veth_loaded_addresses %v", prefix, totalLoaded))
-	allOut = append(allOut, fmt.Sprintf("%veth_total_addresses %v", prefix, len(allWatching)))
+	allOut = append(allOut, fmt.Sprintf("%vaelf_balance_total %0.18f", prefix, total))
+	allOut = append(allOut, fmt.Sprintf("%vaelf_load_seconds %0.2f", prefix, loadSeconds))
+	allOut = append(allOut, fmt.Sprintf("%vaelf_loaded_addresses %v", prefix, totalLoaded))
+	allOut = append(allOut, fmt.Sprintf("%vaelf_total_addresses %v", prefix, len(allWatching)))
 	fmt.Fprintln(w, strings.Join(allOut, "\n"))
 }
 
-//
 // Open the addresses.txt file (name:address)
 func OpenAddresses(filename string) error {
 	file, err := os.Open(filename)
@@ -99,55 +79,70 @@ func OpenAddresses(filename string) error {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		object := strings.Split(scanner.Text(), ":")
-		if common.IsHexAddress(object[1]) {
-			w := &Watching{
-				Name:    object[0],
-				Address: object[1],
-			}
-			allWatching = append(allWatching, w)
+		if len(object) != 2 || !isValidAddress(object[1]) {
+			continue
 		}
+		w := &Watching{
+			Name:    object[0],
+			Address: object[1],
+		}
+		allWatching = append(allWatching, w)
 	}
 	if err := scanner.Err(); err != nil {
 		return err
 	}
-	return err
+	return nil
+}
+
+// TODO: Check if an address is valid
+func isValidAddress(address string) bool {
+	return true
 }
 
 func main() {
-	gethUrl := os.Getenv("GETH")
+	aelfUrl := os.Getenv("AELF_URL")
 	port = os.Getenv("PORT")
 	prefix = os.Getenv("PREFIX")
+	sleepDuration := os.Getenv("SLEEP_DURATION")
 
-	err := OpenAddresses("addresses.txt")
-	if err != nil {
-		panic(err)
+	if aelfUrl == "" || port == "" || prefix == "" || sleepDuration == "" {
+		log.Fatal("Environment variables AELF_URL, PORT, PREFIX, and SLEEP_DURATION must be set")
 	}
 
-	err = ConnectionToGeth(gethUrl)
+	var err error
+	sleepSeconds, err = strconv.Atoi(sleepDuration)
 	if err != nil {
-		panic(err)
+		log.Fatal("SLEEP_DURATION must be an integer")
 	}
 
-	// check address balances
+	err = OpenAddresses("addresses.txt")
+	if err != nil {
+		log.Fatalf("Failed to open addresses.txt: %v", err)
+	}
+
+	err = ConnectToAElf(aelfUrl)
+	if err != nil {
+		log.Fatalf("Failed to connect to AElf node: %v", err)
+	}
+
+	// Check address balances
 	go func() {
 		for {
 			totalLoaded = 0
 			t1 := time.Now()
-			fmt.Printf("Checking %v wallets...\n", len(allWatching))
+			log.Printf("Checking %v wallets...\n", len(allWatching))
 			for _, v := range allWatching {
-				v.Balance = GetEthBalance(v.Address).String()
+				v.Balance = GetAElfBalance(v.Address).String()
 				totalLoaded++
 			}
 			t2 := time.Now()
 			loadSeconds = t2.Sub(t1).Seconds()
-			fmt.Printf("Finished checking %v wallets in %0.0f seconds, sleeping for %v seconds.\n", len(allWatching), loadSeconds, 15)
-			time.Sleep(15 * time.Second)
+			log.Printf("Finished checking %v wallets in %0.2f seconds, sleeping for %v seconds.\n", len(allWatching), loadSeconds, sleepSeconds)
+			time.Sleep(time.Duration(sleepSeconds) * time.Second)
 		}
 	}()
 
-	block := CurrentBlock()
-
-	fmt.Printf("ETHexporter has started on port %v using Geth server: %v at block #%v\n", port, gethUrl, block)
+	log.Printf("AELFexporter has started on port %v using AElf node: %v\n", port, aelfUrl)
 	http.HandleFunc("/metrics", MetricsHttp)
-	panic(http.ListenAndServe("0.0.0.0:"+port, nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, nil))
 }
